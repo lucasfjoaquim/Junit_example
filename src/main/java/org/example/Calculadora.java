2d7f85f6fce2f4b97cb3dbc0012e826c84efbcf4
lucasfjoaquim <lucasfjoaquim@gmail.com>
2024-05-27 14:21:52 -0300

    segundo commit

---
+++ src/main/java/org/example/Calculadora.java
@@ -0,0 +1,13 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Calculadora {
+    public int soma (int x, int y) {
+        return  x + y;
+    }
+
+    public int subtracao (int x, int y) {
+        return  x - y;
+    }
+}
